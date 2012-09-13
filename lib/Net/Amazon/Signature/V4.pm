bed41efa4b612aa972da072c2a224067baf63888
Tim Nordenfur <tim@cpan.org>
2012-09-13 05:12:09 +0000

    Import of TIM/Net-Amazon-Signature-V4-0.11 from CPAN.
    gitpan-cpan-distribution: Net-Amazon-Signature-V4
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         TIM/Net-Amazon-Signature-V4-0.11.tar.gz
    gitpan-cpan-author:       TIM
    gitpan-cpan-maturity:     released

--- lib/Net/Amazon/Signature/V4.pm
+++ lib/Net/Amazon/Signature/V4.pm
@@ -17,11 +17,11 @@ Net::Amazon::Signature::V4 - Implements the Amazon Web Services signature versio
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 
 =head1 SYNOPSIS
