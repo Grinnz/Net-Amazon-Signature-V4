94bf42dc90843b73ca724b5ab0f30a62ae564707
Dan Book <grinnz@grinnz.com>
2017-11-21 22:46:29 -0500

    version 0.17

--- lib/Net/Amazon/Signature/V4.pm
+++ lib/Net/Amazon/Signature/V4.pm
@@ -16,11 +16,11 @@ Net::Amazon::Signature::V4 - Implements the Amazon Web Services signature versio
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =cut
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 
 =head1 SYNOPSIS
