ed75597a69f8da433815371e363f0a6467fc92a6
Dan Book <grinnz@grinnz.com>
2020-10-08 19:22:26 -0400

    version 0.21

--- lib/Net/Amazon/Signature/V4.pm
+++ lib/Net/Amazon/Signature/V4.pm
@@ -16,11 +16,11 @@ Net::Amazon::Signature::V4 - Implements the Amazon Web Services signature versio
 
 =head1 VERSION
 
-Version 0.20
+Version 0.21
 
 =cut
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 
 =head1 SYNOPSIS
